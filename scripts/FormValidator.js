9d13cb4eba9224e64ca9abf0898e66ba37186b77
Victor Abrosimov <vitya.abrosimoff2016@ya.ru>
2020-06-05 14:08:45 +0500

    refactor: в соответствии с требованием ревью

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,17 +1,4 @@
-export { validateEdtitPopap, validatePlusPopap, formObject };
-//редактировать профиль
-const formEdit = document.querySelector(".popap-edit").querySelector('.popap__container');
-//добавить карточку
-const formPlus = document.querySelector(".popap-place").querySelector('.popap__container');
-
-const formObject = {
-  formSelector: '.popap__container',
-  inputSelector: '.popap__input',
-  submitButtonSelector: '.popap__input-save', // кнопка 'сохранить' | 'создать'
-  inactiveButtonClass: 'popap__input-save_inactive',
-  inputErrorClass: 'popap__input-name_type_error', //красная линия под инпутом
-  errorSpanClass: 'popap__input-error_active' //span с текстом ошибки
-};
+export { FormValidator };
 
 class FormValidator {
   constructor (object, form) {
@@ -36,20 +23,6 @@ class FormValidator {
     input.classList.remove(this._inputErrorClass);
     errorElement.classList.remove(this._errorSpanClass);
     errorElement.textContent = '';
-  };
-
-  //очищалка ошибок у инпутов
-  _clearInputError(inputsList, btnElem) {
-    inputsList.forEach((input) => {
-    const errorElement = this._form.querySelector(`#${input.id}-error`);
-      errorElement.textContent = '';
-      input.classList.remove(this._inputErrorClass);
-      //если в форме если хоть один невалидный инпут и кнопка не содержит класс с инактивацией кнопки
-      if (this._hasInvalidInput(inputsList) && !btnElem.classList.contains(this._inactiveButtonClass)) {
-        btnElem.classList.add(this._inactiveButtonClass);
-      } else {
-        btnElem.classList.remove(this._inactiveButtonClass);}
-    })
   };
 
   //проверяем есть ли хоть один невалидный инпут
@@ -80,7 +53,6 @@ class FormValidator {
   _setEventListener() {
     const inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
     const btnElem = this._form.querySelector(this._submitButtonSelector);
-    this._toggleButtonState(inputList, btnElem);
     inputList.forEach((input) => {
       input.addEventListener('input', ()=> {
         this._checkInputValidity(input);
@@ -93,10 +65,4 @@ class FormValidator {
     this._form.addEventListener('submit', (evt) => evt.preventDefault);
     this._setEventListener();
   }
-}
-
-const validateEdtitPopap = new FormValidator(formObject, formEdit);
-validateEdtitPopap.enableValidation();
-
-const validatePlusPopap = new FormValidator(formObject, formPlus);
-validatePlusPopap.enableValidation();
+};
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,39 @@
 import { initialCards } from "./initialCards.js";
 import { Card } from "./Card.js";
-import { validateEdtitPopap, validatePlusPopap, formObject } from "./FormValidator.js";
+import { FormValidator } from "./FormValidator.js";
 export { sectionCards, popapImage, escHandler };
+
+
+/* Уважаемый, Антон, кнопка закрытия форм создания карточки и редактирования профиля НЕ проваливается под форму. Так как
+по ТЗ у нас макет был только для 1280px, студентам дали возможность сделать адаптив так, как они его видят. По моему мнению,
+кнопка закрытия формы расположена удобно для пользователя. Сама форма при этом имеет максимально возможный размер по ширине экрана.
+Если делать так как просите Вы, то форма будет менее читабельной, ибо придется уменьшать сам контейнер с формой, чтобы распололожить
+кнопку закрытия справа. Кнопка расположена под формой только до 500px.
+Поэтому, если у студентов есть свое мнение по поводу замечаний ревью, то я не соглашусь здесь с Вашим замечанием. Спасибо за внимание.*/
+
+
+const formObject = {
+  formSelector: '.popap__container',
+  inputSelector: '.popap__input',
+  submitButtonSelector: '.popap__input-save', // кнопка 'сохранить' | 'создать'
+  inactiveButtonClass: 'popap__input-save_inactive',
+  inputErrorClass: 'popap__input-name_type_error', //красная линия под инпутом
+  errorSpanClass: 'popap__input-error_active' //span с текстом ошибки
+};
+
+//очищалка ошибок у инпутов формы
+function clearInputError(inputsList, form, btnElem, formObject) {
+  inputsList.forEach((input) => {
+  const errorElement = form.querySelector(`#${input.id}-error`);
+    errorElement.textContent = '';
+    input.classList.remove(formObject.inputErrorClass);
+    //если в форме если хоть один невалидный инпут и кнопка не содержит класс с инактивацией кнопки
+    if (!input.validity.valid && !btnElem.classList.contains(formObject.inactiveButtonClass)) {
+      btnElem.classList.add(formObject.inactiveButtonClass);
+    } else {
+      btnElem.classList.remove(formObject.inactiveButtonClass);}
+  })
+};
 
 const sectionCards = document.querySelector(".cards");
 const popapImage = document.querySelector(".popap-image");
@@ -19,6 +51,7 @@ const btnCloseImage = popapImage.querySelector(".popap-image__close");
 btnCloseImage.addEventListener("click", () => popapImage.classList.add("popap-image_closed"));
 
 const popapEdit = document.querySelector(".popap-edit");
+const formEdit = popapEdit.querySelector('.popap__container');
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__signature");
 const nameInput = popapEdit.querySelector(".popap__input-name");
@@ -33,21 +66,21 @@ const configFormEdit = () => {
 function openFormEdit() {
   popapEdit.classList.remove("popap_closed");
   configFormEdit();
-  validateEdtitPopap._clearInputError(inputsFormEdit, btnSave);
+  clearInputError(inputsFormEdit, formEdit, btnSave, formObject);
   document.addEventListener('keydown', escHandler);
+  formEdit.addEventListener('keydown', escHandler);
 };
 
 function closeFormEdit() {
   popapEdit.classList.add("popap_closed");
   configFormEdit();
-  validateEdtitPopap._clearInputError(inputsFormEdit, btnSave);
+  clearInputError(inputsFormEdit, formEdit, btnSave, formObject);
   document.removeEventListener('keydown', escHandler);
+  formEdit.removeEventListener('keydown', escHandler);
 };
 
-(function btnCloseListenFormEdit() {
-  const btnClose = popapEdit.querySelector(".popap__close");
-  btnClose.addEventListener("click", closeFormEdit);
-})();
+const btnCloseFormEdit = popapEdit.querySelector(".popap__close");
+btnCloseFormEdit.addEventListener("click", closeFormEdit);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -65,6 +98,7 @@ const btnEdit = document.querySelector(".profile__btn-edit");
 btnEdit.addEventListener("click", openFormEdit);
 
 const popapPlus = document.querySelector(".popap-place");
+const formPlus = popapPlus.querySelector('.popap__container');
 const namePlaceInput = popapPlus.querySelector(".popap__input-name");
 const linkPlaceInput = popapPlus.querySelector(".popap__input-signature");
 const inputsFormPlus = Array.from(popapPlus.querySelectorAll('.popap__input'));
@@ -76,24 +110,36 @@ const configFormPlus = (formObject) => {
   btnCreate.classList.add(formObject.inactiveButtonClass);
 };
 
+//чекаем инпуты при первом открытии формы
+function checkInputFirstOpen(form, inputList) {
+  inputList.forEach((input) =>{
+    if (!input.validity.valid) {
+      const errorElement = form.querySelector(`#${input.id}-error`);
+      input.classList.add(formObject.inputErrorClass);
+      errorElement.classList.add(formObject.errorSpanClass);
+      errorElement.textContent = input.validationMessage;
+    }
+  })
+}
+
 function openFormPlus() {
   popapPlus.classList.remove("popap_closed");
   configFormPlus(formObject);
-  validatePlusPopap._clearInputError(inputsFormPlus, btnCreate);
+  checkInputFirstOpen(formPlus, inputsFormPlus);
   document.addEventListener('keydown', escHandler);
+  formPlus.addEventListener('keydown', escHandler);
 };
 
 function closeFormPlus() {
   popapPlus.classList.add("popap_closed");
   configFormPlus(formObject);
-  validatePlusPopap._clearInputError(inputsFormPlus, btnCreate);
+  clearInputError(inputsFormPlus, formPlus, btnCreate, formObject);
   document.removeEventListener('keydown', escHandler);
+  formPlus.removeEventListener('keydown', escHandler);
 };
 
-(function btnCloseListenFormPlus() {
-  const btnClose = popapPlus.querySelector(".popap__close");
-  btnClose.addEventListener("click", closeFormPlus);
-})();
+const btnCloseFormPlus = popapPlus.querySelector(".popap__close");
+btnCloseFormPlus.addEventListener("click", closeFormPlus);
 
 function addCard(evt) {
   evt.preventDefault();
@@ -138,3 +184,9 @@ const popaps = document.querySelectorAll('.popap'); //собираем все п
 popaps.forEach((popap) => {
   popap.addEventListener('mousedown', overlayPopap);
 });
+
+const validateEdtitPopap = new FormValidator(formObject, formEdit);
+validateEdtitPopap.enableValidation();
+
+const validatePlusPopap = new FormValidator(formObject, formPlus);
+validatePlusPopap.enableValidation();
