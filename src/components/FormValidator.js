a47f28e68607a2f5a4ed88406099d18570146634
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 01:09:40 +0500

    refactor(FormValidator): semantic code style

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -7,14 +7,14 @@ export class FormValidator {
     this._inputErrorClass = object.inputErrorClass;
     this._errorSpanClass = object.errorSpanClass;
     this._form = document.querySelector(form);
-  }
+  };
 
   _showInputError(input, errorMessage) {
     const errorElement = this._form.querySelector(`#${input.id}-error`);
     input.classList.add(this._inputErrorClass);
     errorElement.classList.add(this._errorSpanClass);
     errorElement.textContent = errorMessage;
-  }
+  };
 
   _hideInputError(input) {
     const errorElement = this._form.querySelector(`#${input.id}-error`);
@@ -27,7 +27,7 @@ export class FormValidator {
   _hasInvalidInput(inputList) {
     return inputList.some((input) => {
       return !input.checkValidity();
-    })
+    });
   };
 
   _toggleButtonState (inputList, btnElem) {
@@ -52,26 +52,26 @@ export class FormValidator {
     const inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
     const btnElem = this._form.querySelector(this._submitButtonSelector);
     inputList.forEach((input) => {
-      input.addEventListener('input', ()=> {
+      input.addEventListener('input', () => {
         this._checkInputValidity(input);
         this._toggleButtonState(inputList, btnElem);
-      })
-    })
+      });
+    });
   };
 
   //очищаем | добавляем ошибки у инпутов формы при ее открытии
   checkInputFirstOpen() {
     //кнопка сохранить | создать
-    const btnElem = this._form.querySelector(this._submitButtonSelector);
     const inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
+    const btnElem = this._form.querySelector(this._submitButtonSelector);
     inputList.forEach((input) => {
       this._checkInputValidity(input);
       this._toggleButtonState(inputList, btnElem)
-    })
-  }
+    });
+  };
 
   enableValidation() {
     this._form.addEventListener('submit', (evt) => evt.preventDefault);
     this._setEventListener();
-  }
+  };
 };
