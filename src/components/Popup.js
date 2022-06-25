2400ece1418eca0e8473ba8e40211b9ac260c1e0
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 01:10:56 +0500

    refactor(Popup): semantic code styles

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,26 +2,31 @@ export class Popup {
   constructor(popapSelector) {
     this._popap = document.querySelector(popapSelector);
     this._handleEscClose = this._handleEscClose.bind(this);
-    this._overlayPopap = this._overlayPopap.bind(this)
-  }
+    this._overlayPopap = this._overlayPopap.bind(this);
+  };
+
   open() {
     this._popap.classList.remove("popap_closed");
     document.addEventListener("keydown", this._handleEscClose);
-    document.addEventListener('mousedown', this._overlayPopap)
-  }
+    document.addEventListener('mousedown', this._overlayPopap);
+  };
+
   close() {
     this._popap.classList.add("popap_closed");
     document.removeEventListener('keydown', this._handleEscClose);
-    document.removeEventListener('mousedown', this._overlayPopap)
-  }
+    document.removeEventListener('mousedown', this._overlayPopap);
+  };
+
   _handleEscClose(evt) {
-    if (evt.key === 'Escape') this.close()
-  }
+    if (evt.key === 'Escape') this.close();
+  };
+
   _overlayPopap(evt) {
-    if (evt.target.classList.contains('popap')) this.close()
-  }
+    if (evt.target.classList.contains('popap')) this.close();
+  };
+
   setEventListeners() {
     const btnClose = this._popap.querySelector(".popap__close");
     btnClose.addEventListener("click", ()=> this.close());
-  }
-}
+  };
+};
