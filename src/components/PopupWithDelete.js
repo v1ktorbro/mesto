67b708627668b28915906a00b1b51ce08d948cdb
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 01:11:34 +0500

    refactor(PopupWithDelete): semantic code style

--- src/components/PopupWithDelete.js
+++ src/components/PopupWithDelete.js
@@ -2,19 +2,18 @@ import { Popup } from "./Popup.js";
 export class PopupWithDelete extends Popup {
   constructor( popapSelector ) {
     super(popapSelector);
-    this._form = this._popap.querySelector(".popap__container")
-  }
+    this._form = this._popap.querySelector(".popap__container");
+  };
 
   setEventListeners() {
-    super.setEventListeners()
-    this._form.addEventListener("submit", (evt)=> {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
-      this._callBackFunc()
-    })
-  }
+      this._callBackFunc();
+    });
+  };
+
   setSubmitAction(action) {
-    this._callBackFunc = action
-  }
-}
-
-
+    this._callBackFunc = action;
+  };
+};
