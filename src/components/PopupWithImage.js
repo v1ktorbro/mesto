d67ff337abee7860bb3b77537ff9b42ad1719ed3
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 01:12:25 +0500

    refactor(PopupWithImage): semantic code styles

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,12 +1,12 @@
 import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
   constructor(popapSelector) {
-    super(popapSelector)
-  }
+    super(popapSelector);
+  };
 
   open(name, link) {
-    super.open()
+    super.open();
     this._popap.querySelector('.popap-image__name').textContent = name;
     this._popap.querySelector('.popap-image__content').src = link;
-  }
-}
+  };
+};
