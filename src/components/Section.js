272bc7494c562588f687c4ee803e9ee2e6c0d518
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 01:12:56 +0500

    refactor(Section): semantic code styles

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,15 +2,15 @@ export class Section {
   constructor({ renderer }, containerSelector) {
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
-  }
+  };
 
   addItem(element) {
     this._container.prepend(element);
-  }
+  };
 
   renderItems(Array, userId) {
     Array.forEach((item) => {
       this._renderer(item, userId);
     });
-  }
-}
+  };
+};
