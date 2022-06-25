a2d58b7ca3560394ec9276e9ef48ffb07a75e880
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 01:12:00 +0500

    refactor(PopupWithForm): semantic code styles

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,25 +5,28 @@ export class PopupWithForm extends Popup {
     this._handleFormSubmit = handleFormSubmit;
     this.formValidator = formValidator;
     this.form = this._popap.querySelector('.popap__container');
-    this._submitForm = this._submitForm.bind(this)
+    this._submitForm = this._submitForm.bind(this);
   };
 
   open() {
     super.open();
     this.formValidator.checkInputFirstOpen();
   }
+
   _submitForm(evt) {
     evt.preventDefault();
     this._handleFormSubmit(this._getInputValues());
   }
+
   setEventListeners() {
     super.setEventListeners();
-    this.form.addEventListener('submit', this._submitForm)
-  }
+    this.form.addEventListener('submit', this._submitForm);
+  };
+
   _getInputValues() {
     this._inputList = this.form.querySelectorAll('.popap__input');
     this._formValues = {};
     this._inputList.forEach(input => this._formValues[input.name] = input.value);
     return this._formValues;
-  }
-}
+  };
+};
